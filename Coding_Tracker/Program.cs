72dc1eddde6fc3685a58a3d426b4ae60329547a3
Mohammad Jabir <mohammadj94@gmail.com>
2023-10-05 10:52:24 -0600

    Add project files.

---
+++ Coding_Tracker/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Coding_Tracker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
